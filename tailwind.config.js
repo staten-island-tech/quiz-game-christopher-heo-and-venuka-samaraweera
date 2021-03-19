0b41382ac13c7c5f8e4c6a7e1d9e7ed19a4ea4ef
heochristopher <christopher.heo1223@gmail.com>
2021-03-18 22:59:24 -0400

    set up tailwind
    styled most of the page but answer section needs to be styled with grid(?)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      background: colors.#ffe182,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
