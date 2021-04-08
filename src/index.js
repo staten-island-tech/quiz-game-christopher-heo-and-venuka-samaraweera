7341113208da5f92d191d6b1ab082d7dcf859ba8
Venuka-Samaraweera <venuka.samaraweera23@sitechhs.com>
2021-04-08 08:49:47 -0400

    hghm

--- src/index.js
+++ src/index.js
@@ -47,3 +47,16 @@ let questions = [
     answer: 3,
   },
 ];
+
+const startButton = document.getElementById("start-btn");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Started");
+  startButton.classList.add("hide");
+}
+
+function setNextQuestion() {}
+
+function selectAnswer() {}
