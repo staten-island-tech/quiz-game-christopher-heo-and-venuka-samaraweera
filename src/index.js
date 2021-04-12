d8eab3f943b202d1a27f6093c83738780fcc575c
Venuka-Samaraweera <venuka.samaraweera23@sitechhs.com>
2021-04-12 16:58:31 -0400

    jfgjg

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { questions } from "./questions"
+import { questions } from "./questions";
 
 const progress = document.querySelector("#progress");
 const question = document.querySelector("#question");
@@ -6,16 +6,3 @@ const choices = Array.from(document.querySelectorAll("#choice"));
 
 let counter = 1;
 let score = 0;
-
-const startButton = document.getElementById("start-btn");
-
-startButton.addEventListener("click", startGame);
-
-function startGame() {
-  console.log("Started");
-  startButton.classList.add("hide");
-}
-
-function setNextQuestion() {}
-
-function selectAnswer() {}
