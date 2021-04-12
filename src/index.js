24fe23c7cf91f45b347dd3cf9654f0df933801d3
heochristopher <christopher.heo1223@gmail.com>
2021-04-12 19:52:44 -0400

    js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-import { questions } from "./questions"
+import { questions } from "./questions";
 
-const progress = document.querySelector("#progress");
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll("#choice"));
-
-let counter = 1;
+const choices = document.querySelectorAll("[id=choice]");
 let score = 0;
+let progress = 0;
 
-const startButton = document.getElementById("start-btn");
+const choiceContainer = document.querySelectorAll("[id=choice-container]");
+console.log(choiceContainer[0].children[0]);
+console.log(choiceContainer[1].children.length);
 
-startButton.addEventListener("click", startGame);
+choiceContainer[1].children[1].setAttribute("data-value", 1);
 
-function startGame() {
-  console.log("Started");
-  startButton.classList.add("hide");
+console.log(choiceContainer[1].children[1].dataset.value);
+
+// for (let i = 0; i < choiceContainer[0].children.length; i++) {
+//   let value = i + 1;
+//   console.log(value);
+//   choiceContainer[0].children[i].setAttribute("data-value", value);
+//   addEventListener("click", function () {
+//     let dataValue = choiceContainer[0].children[i].dataset.value;
+//     console.log(dataValue);
+//   });
+// }
+
+//every time a button is clicked, store the string of the input in an array
+//compare the string to the actual answer at the end
+
+const userAnswers = [];
+
+console.log(choice[1].innerHTML);
+
+for (let i = 0; i < choiceContainer[0].children.length; i++) {
+  addEventListener("click", function () {
+    userAnswers.push(choice[i].innerHTML);
+    console.log(userAnswers);
+  });
 }
-
-function setNextQuestion() {}
-
-function selectAnswer() {}
