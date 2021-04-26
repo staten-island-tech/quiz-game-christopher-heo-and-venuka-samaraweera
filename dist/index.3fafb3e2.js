ffecb0cc599eab75099fe92714e8186e7f252bb5
heochristopher <christopher.heo1223@gmail.com>
2021-04-26 00:08:51 -0400

    actual final iteration
    changed all the for loops to forEach

--- dist/index.3fafb3e2.js
+++ dist/index.3fafb3e2.js
@@ -445,7 +445,7 @@ id) /*: string*/
 //answer key
 let answers = ["Kakashi", "Itachi", "Minato", "Naruto", "Sasuke"];
 
-const MAXQUESTIONS = 5;
+const maxQuestions = answers.length;
 const finalScore = document.getElementById("score"); //HTML score (what's being displayed)
 const images = Array.from(document.querySelectorAll("[id=image]")); //array of all the images
 const submitBtn = document.getElementById("submit-btn");
@@ -459,39 +459,30 @@ const choiceContainer = Array.from(
 
 let userAnswers = []; //empty list for user answer inputs
 
-choices.forEach((choice) => {
+choices.forEach(function (choice) {
   choice.addEventListener("click", function () {
     userAnswers.push(choice.innerHTML); //append the displayed text of the choice to the userAnswers array
-    console.log(choice);
-  });
-  choiceContainer.forEach((container) => {
-    if (container === choice.parentElement) {
-      container.classList.add("hidden"); //whenever a choice is selected, its parent container gets deleted
-      images.forEach((image) => {
-        image.classList.add("hidden"); //whenever a choice is selected, its image gets deleted
-      });
-    }
+    choice.parentElement.classList.add("hidden");
+    choice.parentElement.previousElementSibling.classList.add("hidden");
+    endGame();
   });
 });
 
-//once all the questions are answered, hide the last image and container, calculate score, reveal submit button, and end the game
-if (userAnswers.length === MAXQUESTIONS) {
-  choiceContainer[MAXQUESTIONS - 1].classList.add("hidden");
-  images[MAXQUESTIONS - 1].classList.add("hidden");
-  getScore();
-  submitBtn.classList.remove("hidden");
-  break;
-}
-
 //compare for equality
 function getScore() {
-  for (let i = 0; i < answers.length; i++) {
-    for (let j = 0; j < userAnswers.length; j++) {
-      //iterate through all values of userAnswers array to compare to the answers array, increment score if equal
-      if (answers[i] === userAnswers[j]) {
+  answers.forEach(function (answer) {
+    userAnswers.forEach(function (userAnswer) {
+      if (answer === userAnswer) {
         score++;
       }
-    }
+    });
+  });
+}
+
+function endGame() {
+  if (userAnswers.length === maxQuestions) {
+    submitBtn.classList.remove("hidden");
+    getScore();
   }
 }
 
